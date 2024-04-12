64e436ac528ee8860679996fb771ccfeb5b20a8a
NRnishad <nrnishad106@gmail.com>
2024-04-12 07:53:04 +0530

    add Media

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("projectsBtn").addEventListener("click", function() {
+    alert(" ALL PROJECTS   404 Not Found");
+  });
+
+
+  document.getElementById("galleryBtn").addEventListener("click", function() {
+    alert("VIEW GALLERY   404 Not Found");
+  });
